924ef529360f8c9d18c10de0720b0dbdfd001077
brian_mccabe_mtt <brian.mccabe@mttnow.com>
2016-09-17 17:35:19 +0100

    Created unit test to check if response for joke is not null

--- BuildItBiggerApp/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ BuildItBiggerApp/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,19 +1,11 @@
 package com.udacity.gradle.builditbigger;
 
-import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.util.Pair;
 import android.view.View;
 
-import com.builditbigger.JokeTeller;
 import com.udacity.gradle.builditbigger.domain.JokeManager;
-
-import java.util.concurrent.ExecutionException;
-
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,20 +15,11 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-    try {
-      Log.e("brian", new JokeManager.EndpointsAsyncTask().execute(new Pair<Context, String>(this, "brian")).get());
-    } catch (InterruptedException e) {
-      e.printStackTrace();
-    } catch (ExecutionException e) {
-      e.printStackTrace();
-    }
-
   }
 
   public void tellJoke(View view) {
-    JokeTeller jokeTeller = new JokeTeller();
     Intent intent = new Intent(getApplication(), brianm.builditbiggerandroidlib.MainActivity.class);
-    intent.putExtra(JOKE_KEY, jokeTeller.getJoke());
+    intent.putExtra(JOKE_KEY, JokeManager.getResponse());
     startActivity(intent);
   }
 }
--- BuildItBiggerApp/src/main/java/com/udacity/gradle/builditbigger/domain/JokeManager.java
+++ BuildItBiggerApp/src/main/java/com/udacity/gradle/builditbigger/domain/JokeManager.java
@@ -1,10 +1,6 @@
 package com.udacity.gradle.builditbigger.domain;
 
-import android.content.Context;
 import android.os.AsyncTask;
-import android.util.Log;
-import android.util.Pair;
-import android.widget.Toast;
 
 import com.example.brianm.myapplication.backend.myApi.MyApi;
 import com.google.api.client.extensions.android.http.AndroidHttp;
@@ -13,23 +9,21 @@ import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
 import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
 
 import java.io.IOException;
+import java.util.concurrent.ExecutionException;
 
 /**
  * Created by brianm on 21/08/2016.
  */
 public class JokeManager {
-  public static class EndpointsAsyncTask extends AsyncTask<Pair<Context, String>, Void, String> {
+  public static class EndpointsAsyncTask extends AsyncTask<String, Void, String> {
+
     private MyApi myApiService = null;
-    private Context context;
 
     @Override
-    protected String doInBackground(Pair<Context, String>... params) {
-      if (myApiService == null) {  // Only do this once
+    protected String doInBackground(String... params) {
+      if (myApiService == null) {
         MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
           new AndroidJsonFactory(), null)
-          // options for running against local devappserver
-          // - 10.0.2.2 is localhost's IP address in Android emulator
-          // - turn off compression when running against local devappserver
           .setRootUrl("https://builditbigger-141019.appspot.com/_ah/api/")
           .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
             @Override
@@ -37,13 +31,8 @@ public class JokeManager {
               abstractGoogleClientRequest.setDisableGZipContent(true);
             }
           });
-        // end options for devappserver
-
         myApiService = builder.build();
       }
-
-      context = params[0].first;
-
       try {
         return myApiService.retrieveJoke().execute().getJoke();
       } catch (IOException e) {
@@ -53,9 +42,20 @@ public class JokeManager {
 
     @Override
     protected void onPostExecute(String result) {
-      Toast.makeText(context, result, Toast.LENGTH_LONG).show();
-      Log.e("brian", result);
     }
   }
 
+  public static String getResponse(){
+    String response = "";
+    try {
+      response = new EndpointsAsyncTask().execute().get();
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    } catch (ExecutionException e) {
+      e.printStackTrace();
+    }
+    return response;
+  }
 }
+
+
